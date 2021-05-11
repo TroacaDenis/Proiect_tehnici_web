edff674756831afb780aa38321f5611f0feac52a
TroacaDenis <79210519+TroacaDenis@users.noreply.github.com>
2021-05-11 20:50:25 +0300

    update

--- shop.js
+++ shop.js
@@ -1,85 +1,150 @@
-
+let stock=[];
+let comenzi=[];
 
 function plus(i){
     let items=document.getElementsByClassName("shop-item");
     items[i].children[3].innerHTML=parseInt(items[i].children[3].innerHTML)+1;
 }
 function minus(i){
+    let items=document.getElementsByClassName("shop-item");
     if(items[i].children[3].innerHTML==0)
         return
     items[i].children[3].innerHTML=parseInt(items[i].children[3].innerHTML)-1;
 }
+
+///adauga sau modifica un item din cos cu add_order sau modify_order
 function adauga_in_cos(x){
-    if(qty[x]<items[x].children[3].innerHTML){
-        alert("Eroare: doar " + qty[x] + " bucati ramase");
+    let items=document.getElementsByClassName("shop-item");
+    if(stock[x]<items[x].children[3].innerHTML){
+        alert("Eroare: doar " + stock[x] + " bucati ramase");
         items[x].children[3].innerHTML=0;
     }
     else{
-        if(qty[x]==items[x].children[3].innerHTML){
+        if(stock[x]==items[x].children[3].innerHTML){
             items[x].children[5].removeEventListener("click",function(){adauga_in_cos(x)});
             items[x].children[5].addEventListener("mouseover",function(){items[x].children[5].innerHTML="Indisponibil";});
             items[x].children[5].addEventListener("mouseout",function(){items[x].children[5].innerHTML="Adauga in cos";});
         }
-        orders.push({nume:items[x].children[0].innerHTML, pret:items[x].children[1].innerHTML,cantitate:items[x].children[3].innerHTML,});
-        qty[x]-=items[x].children[3].innerHTML;
+        if(typeof comenzi[x]==='number'){
+            comenzi[x]+=parseInt(items[x].children[3].innerHTML);
+            modify_order(x);
+        }
+        else{
+            comenzi[x]=parseInt(items[x].children[3].innerHTML); 
+            add_order(x);
+        }        
+        stock[x]-=items[x].children[3].innerHTML;
         items[x].children[3].innerHTML=0;
     }
 }
-function create_cos(){
-    let j;
-    for(j=0;j<orders.length;j++){
-        let aux=document.createElement("div");
-        aux.className="cos-item";
-        aux.innerHTML='abcdsadwa';
-        document.getElementById("main_cos").appendChild(aux);
+function add_order(x){
+    let items=document.getElementsByClassName("shop-item");
+    let new_order={
+        id: x+1,
+        qty: comenzi[x],
+        info: items[x].children[0].innerHTML,
+        pret: parseInt(items[x].children[1].innerHTML.substr(0,items[x].children[1].innerHTML.length-11))
     }
+    fetch('http://localhost:3000/orders', {
+        method: 'post', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(new_order)
+    }).then(function(response) {
+        console.log(response);
+    })
+}
+function modify_order(x){
+    let items=document.getElementsByClassName("shop-item");
+    let new_order={
+        id: x+1,
+        qty: comenzi[x],
+        info: items[x].children[0].innerHTML,
+        pret: parseInt(items[x].children[1].innerHTML.substr(0,items[x].children[1].innerHTML.length-11))
+    }
+    let aux=x+1;
+    fetch('http://localhost:3000/orders/'+aux, {
+        method: 'put', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(new_order)
+    }).then(function(response) {
+        console.log(response);
+    })
 }
 
+///afiseaza bunurile ce pot fi cumparate
+function create_orders(){
+    fetch('http://localhost:3000/products', {
+    method: 'get'
+    }).then(function(response) {
+        response.json().then((data) => {
+            let i;
+            for(i=0;i<data.length;i++){
+                let x=i;
+                stock.push(data[x].qty);
+                let auxdiv=document.createElement("div");
+                auxdiv.className="shop-item";
 
+                let auxdescriere=document.createElement("p");
+                auxdescriere.innerHTML=data[x].info;
+                auxdescriere.className="descriere";
+                auxdiv.appendChild(auxdescriere);
 
+                let auxpret=document.createElement("p");
+                auxpret.innerHTML=data[x].pret+ ' lei bucata';
+                auxpret.className="pret";
+                auxdiv.appendChild(auxpret);
 
-var xhttp = new XMLHttpRequest();
-xhttp.open("GET", "http://localhost:3000/products", true);
-xhttp.onload=function(){
-    var info=JSON.parse(xhttp.responseText);
-    let i;
-    for(i=0;i<info.length;i++){
-        let x=i;
-        let auxdiv=document.createElement("div");
-        auxdiv.className="shop-item";
+                let auxminus=document.createElement("button");
+                auxminus.innerHTML="-";
+                auxminus.className="minus";
+                auxminus.addEventListener("click",function(){minus(x)});
+                auxdiv.appendChild(auxminus);
 
-        let auxdescriere=document.createElement("p");
-        auxdescriere.innerHTML=info[i].info;
-        auxdescriere.className="descriere";
-        auxdiv.appendChild(auxdescriere);
+                let auxnumar=document.createElement("p");
+                auxnumar.innerHTML=0;
+                auxnumar.className="numar";
+                auxdiv.appendChild(auxnumar);
 
-        let auxpret=document.createElement("p");
-        auxpret.innerHTML=info[i].pret;
-        auxpret.className="pret";
-        auxdiv.appendChild(auxpret);
+                let auxplus=document.createElement("button");
+                auxplus.innerHTML="+";
+                auxplus.className="plus";
+                auxplus.addEventListener("click",function(){plus(x)});
+                auxdiv.appendChild(auxplus);
 
-        let auxminus=document.createElement("button");
-        auxminus.innerHTML="-";
-        auxminus.className="minus";
-        auxdiv.appendChild(auxminus);
+                let auxadauga=document.createElement("button");
+                auxadauga.innerHTML="adauga";
+                auxadauga.className="adauga";
+                if(stock[x]==0){
+                    auxadauga.addEventListener("mouseover",function(){auxadauga.innerHTML="Indisponibil";});
+                    auxadauga.addEventListener("mouseout",function(){auxadauga.innerHTML="Adauga in cos";});
+                }
+                else
+                    auxadauga.addEventListener("click",function(){adauga_in_cos(x)});
+                auxdiv.appendChild(auxadauga);
 
-        let auxnumar=document.createElement("p");
-        auxnumar.innerHTML=0;
-        auxnumar.className="numar";
-        auxdiv.appendChild(auxnumar);
+                document.getElementById("main").appendChild(auxdiv);
+            }
+        })
+    })
+}
+create_orders();
 
-        let auxplus=document.createElement("button");
-        auxplus.innerHTML="+";
-        auxplus.className="plus";
-        auxplus.addEventListener("click",function(){plus(x)});
-        auxdiv.appendChild(auxplus);
-
-        let auxadauga=document.createElement("button");
-        auxadauga.innerHTML="adauga";
-        auxadauga.className="adauga";
-        auxdiv.appendChild(auxadauga);
-
-        document.getElementById("main").appendChild(auxdiv);
-    }
-};
-xhttp.send(); 
+///verifica daca exista deja elemente in cos
+function get_cos_existent(){
+    fetch('http://localhost:3000/orders', {
+    method: 'get'
+    }).then(function(response) {
+        response.json().then((data) => {
+            let i;
+            for(i=0;i<data.length;i++){
+                let x=i;
+                comenzi[data[x].id-1]=data[x].qty;
+            }
+        })
+    })
+}
+get_cos_existent();
---
+++ shopping_cart.js
@@ -0,0 +1,53 @@
+let ids=[];
+function create_cart(){
+    fetch('http://localhost:3000/orders', {
+    method: 'get'
+    }).then(function(response) {
+        response.json().then((data) => {
+            let i;
+            for(i=0;i<data.length;i++){
+                let x=i;
+                ids.push(data[x].id);
+                let p=document.getElementById("pret_total");
+                p.innerHTML='Pret total: '+(parseInt(p.innerHTML.substr(12,p.innerHTML.length))+data[x].pret*data[x].qty);
+                let auxdiv=document.createElement("div");
+                auxdiv.className="shop-item";
+
+                let auxqty=document.createElement("p");
+                auxqty.innerHTML=data[x].qty;
+                auxqty.className="cantitate";
+                auxdiv.appendChild(auxqty);
+
+                let auxdescriere=document.createElement("p");
+                auxdescriere.innerHTML=data[x].info;
+                auxdescriere.className="descriere";
+                auxdiv.appendChild(auxdescriere);
+
+                let auxpret=document.createElement("p");
+                auxpret.innerHTML=data[x].pret*data[x].qty +' lei';
+                auxpret.className="pret";
+                auxdiv.appendChild(auxpret);
+
+                document.getElementById("main_cos").appendChild(auxdiv);
+            }
+        })
+    })
+}
+create_cart();
+
+function delete_cart(){
+    let items=document.getElementsByClassName("shop-item");
+    let i;
+    for(i=0;i<ids.length;i++){
+        let x=i;
+        fetch('http://localhost:3000/orders/' + ids[x], {
+            method: 'delete',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function(response) {
+            window.location.reload(); 
+        })
+    }
+}
+window.addEventListener('beforeunload',delete_cart());
